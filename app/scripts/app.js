393984bf041a27dea7ab53e5c7f93072f98c668c
itay <itayoded1@gmail.com>
2015-11-14 19:32:23 +0200

    add angular routing, the main content will change by clicking on the list item

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,20 @@
 'use strict';
 
-angular.module('500techtest', [])
+angular.module('500techtest', ['ngRoute'])
 
     .constant('version', 'v0.1.0')
+    
+    .config(function($locationProvider,$routeProvider) {
+
+      $locationProvider.html5Mode(false);
+      $routeProvider
+        .when('/rss/:urlId', {
+            templateUrl: 'views/content.html',
+            controller: 'rssFeedCtrl'
+        })
+        .otherwise({
+            template: 'Please feed me with url'
+        });
+
+    })
     
---
+++ app/scripts/controllers/rssFeed.js
@@ -0,0 +1,7 @@
+'use strict';
+
+angular.module('500techtest')
+.controller('rssFeedCtrl',function($scope,$routeParams){
+    $scope.a = $routeParams.urlId
+
+})
